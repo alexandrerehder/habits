3043f409385244e5d429a84d369decc241d8bfd4
Alexandre Rehder <alexandrerehder@hotmail.com>
2023-01-21 05:58:40 -0300

    Interface web / mobile

---
+++ mobile/src/lib/dayjs.ts
@@ -0,0 +1,4 @@
+import dayjs from "dayjs";
+import "dayjs/locale/pt-bt";
+
+dayjs.locale("pt-bt");
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -7,8 +7,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "#09090A"
+        background: '#09090A'
       },
+      gridTemplateRows: {
+        7: 'repeat(7, minmax(0, 1fr))'
+      }
     },
   },
   plugins: [],
